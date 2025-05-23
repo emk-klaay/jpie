21ad83bbd416154c166723468bf7e7ad6efb1925
Emil Kampp <emk@klaay.com>
2025-05-23 15:25:58 +0200

    test: add main JPie module test coverage

---
+++ spec/jpie_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe JPie do
+  it 'has a version number' do
+    expect(JPie::VERSION).not_to be nil
+    expect(JPie::VERSION).to match(/\A\d+\.\d+\.\d+\z/)
+  end
+
+  describe 'module structure' do
+    it 'defines all expected classes' do
+      expect(defined?(JPie::Resource)).to be_truthy
+      expect(defined?(JPie::Serializer)).to be_truthy
+      expect(defined?(JPie::Deserializer)).to be_truthy
+      expect(defined?(JPie::Controller)).to be_truthy
+      expect(defined?(JPie::Configuration)).to be_truthy
+      expect(defined?(JPie::Errors)).to be_truthy
+    end
+
+    it 'has proper class hierarchy' do
+      expect(JPie::Errors::BadRequestError.ancestors).to include(JPie::Errors::Error)
+      expect(JPie::Controller).to be_a(Module)
+    end
+  end
+
+  describe 'autoloading' do
+    it 'loads all components when required' do
+      # This test ensures that require 'jpie' loads all necessary components
+      expect { JPie::Resource }.not_to raise_error
+      expect { JPie::Serializer }.not_to raise_error
+      expect { JPie::Deserializer }.not_to raise_error
+      expect { JPie::Controller }.not_to raise_error
+      expect { JPie::Configuration }.not_to raise_error
+      expect { JPie::Errors::Error }.not_to raise_error
+    end
+  end
+end 
