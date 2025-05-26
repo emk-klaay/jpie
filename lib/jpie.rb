996fa32650a31a4380d358a12a42bb0e9a7aa0bd
Emil Kampp <emk@klaay.com>
2025-05-27 01:10:49 +0200

    Update JPie module to load RSpec support conditionally
    - Change Resource, Client, and Errors from autoload to require_relative
    
    - Add conditional loading of RSpec support when RSpec is defined

--- lib/jpie.rb
+++ lib/jpie.rb
@@ -3,14 +3,18 @@
 require 'active_support'
 require 'active_support/core_ext'
 require 'jpie/version'
+require_relative 'jpie/resource'
+require_relative 'jpie/client'
+require_relative 'jpie/errors'
+
+# Load RSpec support if RSpec is defined
+require_relative 'jpie/rspec' if defined?(RSpec)
 
 module JPie
-  autoload :Resource, 'jpie/resource'
   autoload :Serializer, 'jpie/serializer'
   autoload :Deserializer, 'jpie/deserializer'
   autoload :Controller, 'jpie/controller'
   autoload :Configuration, 'jpie/configuration'
-  autoload :Errors, 'jpie/errors'
   autoload :Routing, 'jpie/routing'
 
   class << self
